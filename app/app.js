d00d57dc0db275dad6bb7cdf4876445f12dcf0a4
nadunindunil <nadun1indunil@gmail.com>
2015-12-14 17:56:51 +0530

    routing was done

--- app/app.js
+++ app/app.js
@@ -1,12 +1,19 @@
 'use strict';
 
 // Declare app level module which depends on views, and components
-angular.module('myApp', [
-  'ngRoute',
-  'myApp.view1',
-  'myApp.view2',
-  'myApp.version'
-]).
-config(['$routeProvider', function($routeProvider) {
-  $routeProvider.otherwise({redirectTo: '/view1'});
-}]);
+var app = angular.module('myApp',['ngRoute']);
+
+app.config(function($routeProvider){
+      $routeProvider.
+          when('/home',{
+              templateUrl:'templates/home.html'
+          }).
+          when('/students', {
+              templateUrl: 'templates/students.html'
+
+          }).
+          otherwise({
+              redirectTo: '/home'
+          });
+
+});
---
+++ app/controllers/controllers.js
@@ -0,0 +1,5 @@
+/**
+ * Created by NADUN on 12/14/2015.
+ */
+
+
